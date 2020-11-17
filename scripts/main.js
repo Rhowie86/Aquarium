b3ce1d513ad3d48215b214c86bd4141d2c2395f2
Rhowie86 <ronaldnhowie@gmail.com>
2020-11-17 11:53:50 -0600

    cleaned it up

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,14 @@ const fishObj [
     length: 8
     harvestloc: "Polluted River"
     food: "Radioactive Sludge"
-}
+},
 {
     name: "Ripster"
     species: "Human/Shark"
     length: 10
     harvestloc: "Fission City"
     food: "Hamburgers"
-}
+},
 {
     
     name: 
